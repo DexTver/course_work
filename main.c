727e8e9c87cebabee5e7fcaf1fab08027c1a24f1
User <ivan.sharapov.05@yandex.ru>
2023-12-04 23:49:20 +0300

    Code without testing

--- main.c
+++ main.c
@@ -1,6 +1,174 @@
-// 52
+#include <stdio.h>
 
+#define HASH_MOD 1000000009  // hash module
+#define HASH_B 43  // hash base
+#define MAXLEN_N 128  // file name
+#define MAXLEN_K 128  // keyword
+#define MAXLEN_D 64  // delimiters
+#define MAXLEN_S 512 // text line
+#define MAXLINES 512  // lines
+
+int is_delimiter(char c, char *delimiters) {
+    char *x;
+    int ans = 0;
+    x = delimiters;
+    while (*x != '\0') {
+        if (c == *x) ans = 1;
+        ++x;
+    }
+    return ans;
+}
 
 int main() {
+    // Block of variables
+    int r;
+    int t;
+    char type_of_input;
+    char filename[MAXLEN_N];
+    FILE *f;
+    int cnt_of_keywords;
+    char keyword[MAXLEN_K];
+    char *x;
+    long long hash_of_word;
+    char delimiters[MAXLEN_D];
+    int flag;
+    int lines;
+    char text[MAXLINES][MAXLEN_S];
+    char *start;
+    int min_line;
+    int cnt_words_bef;  // count of found words before
+    int cnt_words_now;  //
+    char *min_word[MAXLINES][2];
+    char type_of_output;
+
+    // Block of initialization
+    r = HASH_MOD;
+    t = HASH_B;
+    flag = 1;
+    lines = 0;
+    cnt_words_bef = 0;
+
+    // Block of input
+    printf("Input from file or console? (f/c)\n");
+    type_of_input = getchar();
+    while (type_of_input != 'f' && type_of_input != 'c') {
+        printf("Something went wrong! Please enter \'f\' or \'c\':\n");
+        type_of_input = getchar();
+    }
+    if (type_of_input == 'f') {
+        printf("Please enter the file name (limit: %i chars):\n", MAXLEN_N);
+        scanf("%s", filename);
+        f = fopen(filename, "r");
+        while (f == NULL) {
+            printf("Something went wrong! Perhaps such a file does not exist."
+                   "\nPlease enter the file name again:\n");
+            scanf("%s", filename);
+            f = fopen(filename, "r");
+        }
+    } else {
+        f = stdin;
+    }
+
+    if (type_of_input == 'c') printf("Please enter the count of keywords:\n");
+    fscanf(f, "%i", &cnt_of_keywords);
+
+    int keysh[cnt_of_keywords][2];  // ?!
+
+    if (type_of_input == 'c') printf("Please enter the keywords:\n");
+    for (int i = 0; i < cnt_of_keywords; ++i) {
+        fscanf(f, "%s", keyword);
+        x = keyword;
+        hash_of_word = 0;
+        while (*x != '\0') {
+            hash_of_word = (hash_of_word * t + (int) *x) % r;
+            ++x;
+        }
+        keysh[i][0] = hash_of_word;
+        keysh[i][1] = 0;
+    }
+
+    if (type_of_input == 'c') printf("Please enter the line of delimiter characters (limit: %i chars):\n", MAXLEN_D);
+    fgets(delimiters, MAXLEN_D, f);  //  остыль, чтобы считать \n
+    fgets(delimiters, MAXLEN_D, f);
+
+    if (type_of_input == 'c') printf("Please enter the lines of text (limit of line\'s length: %i; limit\n"
+               "count of lines: %i):\n", MAXLEN_S, MAXLINES);
+
+    // Block of main logic
+    while (flag == 1 && fgets(text[lines], MAXLEN_S, f)) {
+        x = text[lines];
+        start = text[lines];
+        hash_of_word = 0;
+        min_line = MAXLEN_S;
+        do {
+            if (is_delimiter(*x, delimiters) == 1) {
+                if (hash_of_word != 0) {
+                    for (int i = 0; i < cnt_of_keywords; ++i) {
+                        if (hash_of_word == keysh[i][0]) {
+                            ++keysh[i][1];
+                        }
+                    }
+                }
+                hash_of_word = 0;
+                if (x - start != 0 && min_line > x - start) {
+                    min_line = x - start;
+                    min_word[lines][0] = start;
+                    min_word[lines][1] = x;
+                }
+                start = x + 1;
+            } else {
+                hash_of_word = (hash_of_word * t + (int) *x) % r;
+            }
+            ++x;
+        } while (*x != '\0');
+        flag = 0;
+        cnt_words_now = 0;
+        for (int i = 0; i < cnt_of_keywords; ++i) {
+            if (keysh[i][1] == 0) flag = 1;
+            cnt_words_now += keysh[i][1];
+        }
+        if (cnt_words_now == cnt_words_bef) {  // ≈сли в строке не было кл. слов, то минимальное слово обнул€ем
+            min_word[lines][0] = text[lines];
+            min_word[lines][1] = text[lines];
+        }
+        cnt_words_bef = cnt_words_now;
+        lines++;
+    }
+    fclose(f);
+
+    // Block of output
+    printf("Output to file or console? (f/c)\n");
+    getchar();
+    type_of_output = getchar();
+    while (type_of_output != 'f' && type_of_output != 'c') {
+        printf("Something went wrong! Please enter \'f\' or \'c\':\n");
+        type_of_output = getchar();
+    }
+    if (type_of_output == 'f') {
+        printf("Please enter the file name (limit: %i chars):\n", MAXLEN_N);
+        scanf("%s", filename);
+        f = fopen(filename, "w");
+        while (f == NULL) {
+            printf("Something went wrong! Please enter the file name again:\n");
+            scanf("%s", filename);
+            f = fopen(filename, "w");
+        }
+    } else {
+        printf("Processed text:\n");
+        f = stdout;
+    }
+    for (int i = 0; i < lines; ++i) {
+        x = text[i];
+        while (*x != '\n') {
+            if (x == min_word[i][0]) {
+                x = min_word[i][1];
+                min_word[i][0] = NULL;
+            }
+            fprintf(f, "%c", *x);
+            ++x;
+        }
+        fprintf(f, "\n");
+    }
+    fclose(f);
     return 0;
 }
