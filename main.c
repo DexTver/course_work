d9f4f60c56700b277a9e396c0f7f3523fa6369ee
User <ivan.sharapov.05@yandex.ru>
2023-12-07 20:20:57 +0300

    Add a second hash

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 #define HASH_MOD 1000000009  // hash module
-#define HASH_B 43  // hash base
+#define HASH_B1 43  // hash base 1
+#define HASH_B2 47  // hash base 2
 #define MAXLEN_N 128  // file name
 #define MAXLEN_K 128  // keyword
 #define MAXLEN_D 64  // delimiters
@@ -23,13 +24,15 @@ int main() {
     // Block of variables
     int r;
     int t;
+    int b;
     char type_of_input;
     char filename[MAXLEN_N];
     FILE *f;
     int cnt_of_keywords;
     char keyword[MAXLEN_K];
     char *x;
-    long long hash_of_word;
+    long long hash_of_word1;
+    long long hash_of_word2;
     char delimiters[MAXLEN_D];
     int flag;
     int lines;
@@ -43,7 +46,8 @@ int main() {
 
     // Block of initialization
     r = HASH_MOD;
-    t = HASH_B;
+    t = HASH_B1;
+    b = HASH_B2;
     flag = 1;
     lines = 0;
     cnt_words_bef = 0;
@@ -72,41 +76,44 @@ int main() {
     if (type_of_input == 'c') printf("Please enter the count of keywords:\n");
     fscanf(f, "%i", &cnt_of_keywords);
 
-    int keysh[cnt_of_keywords][2];  // ?!
+    int keysh[cnt_of_keywords][3];  // ?!
 
     if (type_of_input == 'c') printf("Please enter the keywords:\n");
     for (int i = 0; i < cnt_of_keywords; ++i) {
         fscanf(f, "%s", keyword);
         x = keyword;
-        hash_of_word = 0;
+        hash_of_word1 = 0;
+        hash_of_word2 = 0;
         while (*x != '\0') {
-            hash_of_word = (hash_of_word * t + (int) *x) % r;
+            hash_of_word1 = (hash_of_word1 * t + (int) *x) % r;
+            hash_of_word2 = (hash_of_word2 * b + (int) *x) % r;
             ++x;
         }
-        keysh[i][0] = hash_of_word;
-        keysh[i][1] = 0;
+        keysh[i][0] = hash_of_word1;
+        keysh[i][1] = hash_of_word2;
+        keysh[i][2] = 0;
     }
 
     if (type_of_input == 'c') printf("Please enter the line of delimiter characters (limit: %i chars):\n", MAXLEN_D);
     fgets(delimiters, MAXLEN_D, f);  // Êîñòûëü, ÷òîáû ñ÷èòàòü \n
     fgets(delimiters, MAXLEN_D, f);
 
-    if (type_of_input == 'c') printf("Please enter the lines of text (limit of line\'s length: %i; limit\n"
+    if (type_of_input == 'c')
+        printf("Please enter the lines of text (limit of line\'s length: %i; limit\n"
                "count of lines: %i):\n", MAXLEN_S, MAXLINES);
 
     // Block of main logic
     while (flag == 1 && fgets(text[lines], MAXLEN_S, f)) {
         x = text[lines];
         start = text[lines];
-        hash_of_word = 0;
+        hash_of_word1 = 0;
+        hash_of_word2 = 0;
         min_line = MAXLEN_S;
         while (*x != '\0') {
             if (is_delimiter(*x, delimiters) == 1) {
-                if (hash_of_word != 0) {
-                    for (int i = 0; i < cnt_of_keywords; ++i) {
-                        if (hash_of_word == keysh[i][0]) {
-                            ++keysh[i][1];
-                        }
+                for (int i = 0; i < cnt_of_keywords; ++i) {
+                    if (hash_of_word1 == keysh[i][0] && hash_of_word2 == keysh[i][1]) {
+                        ++keysh[i][2];
                     }
                     hash_of_word = 0;
                 }
@@ -116,16 +123,19 @@ int main() {
                     min_word[lines][1] = x;
                 }
                 start = x + 1;
+                hash_of_word1 = 0;
+                hash_of_word2 = 0;
             } else {
-                hash_of_word = (hash_of_word * t + (int) *x) % r;
+                hash_of_word1 = (hash_of_word1 * t + (int) *x) % r;
+                hash_of_word2 = (hash_of_word2 * b + (int) *x) % r;
             }
             ++x;
         }
         flag = 0;
         cnt_words_now = 0;
         for (int i = 0; i < cnt_of_keywords; ++i) {
-            if (keysh[i][1] == 0) flag = 1;
-            cnt_words_now += keysh[i][1];
+            if (keysh[i][2] == 0) flag = 1;
+            cnt_words_now += keysh[i][2];
         }
         if (cnt_words_now == cnt_words_bef) {  // Åñëè â ñòðîêå íå áûëî êë. ñëîâ, òî ìèíèìàëüíîå ñëîâî îáíóëÿåì
             min_word[lines][0] = text[lines];
